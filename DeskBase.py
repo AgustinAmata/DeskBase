e277c72aeabd5f0b0c24d529aed123ca75b99669
AgustinAmata <agus.amata2002@gmail.com>
2025-04-24 19:57:21 +0200

    Update DeskBase.py

--- DeskBase.py
+++ DeskBase.py
@@ -1,2 +1,12 @@
+from src.ui_main import MainApp
+
 if __name__ == "__main__":
-    pass
+    app = MainApp()
+    
+    window_width = 1080
+    window_height = 720
+    position_right = app.winfo_screenwidth()//2 - window_width/2
+    position_down = app.winfo_screenheight()//2 - window_height/2
+    app.geometry(f"{window_width}x{window_height}+{position_right}+{position_down}")
+
+    app.mainloop()
