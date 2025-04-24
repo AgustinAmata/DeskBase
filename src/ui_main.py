69cdf14f40e0fa1fd43251422ec86d95561c7ace
AgustinAmata <agus.amata2002@gmail.com>
2025-04-24 19:58:11 +0200

    Update ui_main.py
    Copied info from DeskBase_old.py

---
+++ src/ui_main.py
@@ -0,0 +1,21 @@
+import customtkinter as ctk
+from src.db_manager import DBManager
+from src.ui_components.menubar import Menubar
+# from ui_components.db_tab import DBTab
+
+ctk.set_appearance_mode("light")
+
+class MainApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1200x700")
+        self.title("DeskBase")
+        self.db = DBManager()
+        self.menubar = Menubar(self)
+        # self.maintab = DBTab(self)
+        self.protocol("WM_DELETE_WINDOW", self.close_everything)
+
+    def close_everything(self):
+        if self.db.cnx:
+            self.db.close_connection()
+        self.destroy()
