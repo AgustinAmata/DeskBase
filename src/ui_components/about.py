19842290b43f9fe8b9e0329f102660e34a6cf488
AgustinAmata <agus.amata2002@gmail.com>
2025-04-25 19:57:05 +0200

    Update about.py

---
+++ src/ui_components/about.py
@@ -0,0 +1,27 @@
+import customtkinter as ctk
+
+class About(ctk.CTkToplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.title("Acerca de")
+        self.geometry("300x200")
+        self.wm_transient(master)
+        self.about_frame = ctk.CTkFrame(self, fg_color="transparent")
+
+        ctk.CTkLabel(self.about_frame,
+                     text='''DeskBase:
+Sistema de inventario de equipos''',
+                     font=("Arial", 14, "bold")).pack()
+
+        ctk.CTkLabel(self.about_frame,
+                text='''Desarrollado en Python por
+Agustín Amata
+y
+Francisco Mira-Perceval Maiquez''',
+                font=("Arial", 10)).pack(pady=(20,0))
+
+        ctk.CTkLabel(self.about_frame,
+                text="MIT License",
+                font=("Arial", 12, "bold")).pack(pady=(20,0))
+
+        self.about_frame.pack(anchor="center", expand=True)
