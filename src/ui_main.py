f02bfaa6eb2499579553363da61af79b18479e82
AgustinAmata <agus.amata2002@gmail.com>
2025-04-27 23:35:46 +0200

    Update ui_main.py
    Reconstructed MainTab and its contents

--- src/ui_main.py
+++ src/ui_main.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 from src.db_manager import DBManager
 from src.ui_components.menubar import Menubar
-# from ui_components.db_tab import DBTab
+from src.ui_components.db_tab import DBTab
 
 ctk.set_appearance_mode("light")
 
@@ -11,11 +11,23 @@ class MainApp(ctk.CTk):
         self.geometry("1200x700")
         self.title("DeskBase")
         self.db = DBManager()
-        self.menubar = Menubar(self, self.db)
-        # self.maintab = DBTab(self)
+        self.maintab = MainTab(self, self.db)
+        self.db_tab = self.maintab.devices_obj
+        self.menubar = Menubar(self, self.db, self.db_tab)
         self.protocol("WM_DELETE_WINDOW", self.close_everything)
 
     def close_everything(self):
         if self.db.cnx:
             self.db.close_connection(show_msg=False)
         self.destroy()
+
+class MainTab(ctk.CTkTabview):
+    def __init__(self, master, db: DBManager):
+        super().__init__(master)
+        self.master = master
+        self.db = db
+        self.devices_tab = self.add("Lista de equipos")
+        self.devices_obj = DBTab(self.devices_tab, self.db)
+        self.stats_tab = self.add("Estadísticas")
+
+        self.pack(fill="both", expand=True)
