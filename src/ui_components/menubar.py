4397e651e6d2f2a047e212ba1e5979569d96558d
AgustinAmata <agus.amata2002@gmail.com>
2025-04-27 23:34:53 +0200

    Update menubar.py
    Changed function calling. Now, when the connection is closed from the Menu, it also deletes the contents of DBView

--- src/ui_components/menubar.py
+++ src/ui_components/menubar.py
@@ -5,27 +5,29 @@ from src.logic import change_appearance, convert_to_excel, info_clear
 from src.data_controller import db_deleterow, db_loadrowinfo, db_showentries
 from src.ui_components.about import About
 from src.ui_components.db_window import DBWindow
+from src.ui_components.db_tab import DBTab
 
 class Menubar(tk.Menu):
-    def __init__(self, master, db: DBManager):
+    def __init__(self, master, db: DBManager, db_tab: DBTab):
         super().__init__(master, tearoff=0)
         self.master = master
         self.db = db
+        self.db_tab = db_tab
         self.m1 = tk.Menu(self, tearoff=0)
         self.m1.add_command(label="Conectar a base de datos", command=self.create_dbwindow)
-        self.m1.add_command(label="Desconectar base de datos", command=self.db.close_connection)
+        self.m1.add_command(label="Desconectar base de datos", command= lambda: self.close_cnx())
         self.m1.add_separator()
-        self.m1.add_command(label="Exportar a Excel", command=convert_to_excel)
+        self.m1.add_command(label="Exportar a Excel", command=lambda: convert_to_excel(self))
         master.config(menu=self)
         self.add_cascade(label="Archivo", menu=self.m1)
 
         self.m2 = tk.Menu(self, tearoff=0)
-        self.m2.add_command(label="Limpiar campos")
+        self.m2.add_command(label="Limpiar campos", command=lambda: info_clear(self.db_tab.info.entries))
         self.m2.add_separator()
-        self.m2.add_command(label="Eliminar equipo")
-        self.m2.add_command(label="Cargar equipo")
+        self.m2.add_command(label="Eliminar equipo", command=lambda: db_deleterow(self.db_tab))
+        self.m2.add_command(label="Cargar equipo", command=lambda: db_loadrowinfo(self.db_tab))
         self.m2.add_separator()
-        self.m2.add_command(label="Mostrar equipos")
+        self.m2.add_command(label="Mostrar equipos", command=lambda: db_showentries(self.db_tab))
         self.add_cascade(label="Editar", menu=self.m2)
 
         self.m3 = tk.Menu(self, tearoff=0)
@@ -44,7 +46,7 @@ class Menubar(tk.Menu):
 
     def create_dbwindow(self):
         if self.dbwin is None or not self.dbwin.winfo_exists():
-            self.dbwin = DBWindow(self.master, self.db)
+            self.dbwin = DBWindow(self.master, self.db, self.db_tab)
         else:
             self.dbwin.focus()
 
@@ -53,3 +55,7 @@ class Menubar(tk.Menu):
             self.about = About(self.master)
         else:
             self.about.focus()
+
+    def close_cnx(self):
+        self.db.close_connection()
+        self.db_tab.table.delete(*self.db_tab.table.get_children())
