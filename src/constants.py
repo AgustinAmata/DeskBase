a35ef5950cbe52d7b9ae6bf8ac18afd64f8d3b80
AgustinAmata <agus.amata2002@gmail.com>
2025-05-08 23:56:33 +0200

    Update constants.py
    New columns added to DeskBase table: time and user that made the last modification to an entry; GPU model and RAM capacity

--- src/constants.py
+++ src/constants.py
@@ -2,14 +2,19 @@ LABELS = [
     "Tipo*", "Marca*", "Modelo*", "Serial*", 
     "Fecha de Adquisición", "Estado",
     "Ubicación", "Sistema Operativo", "Modelo Placa Base", "Tarjeta Gráfica",
-    "Tipo de Almacenamiento", "Capacidad de Almacenamiento", "Memoria RAM", "Capacidad de RAM"
+    "Modelo Tarjeta Gráfica", "RAM Tarjeta Gráfica (GB)",
+    "Tipo de Almacenamiento", "Capacidad de Almacenamiento (GB)", "Memoria RAM",
+    "Capacidad de RAM (GB)", "Última modificación por", "Fecha última modificación"
 ]
 
-TABLE_LABELS = ["nombre", "marca", "modelo", "serial", "fecha_adquisicion", "estado",
+TABLE_LABELS = [
+                "nombre", "marca", "modelo", "serial", "fecha_adquisicion", "estado",
                 "ubicacion", "sistema_operativo", "modelo_placa_base", "tarjeta_grafica",
-                "tipo_almacenamiento", "capacidad_almacenamiento", "memoria_ram", "capacidad_ram"]
+                "modelo_tarjeta_grafica", "ram_tarjeta", "tipo_almacenamiento",
+                "capacidad_almacenamiento", "memoria_ram", "capacidad_ram"
+]
 
-LABEL_CONVERSION = {LABELS[i]:TABLE_LABELS[i] for i in range(14)}
+LABEL_CONVERSION = {LABELS[i]:TABLE_LABELS[i] for i in range(16)}
 
 TREE_TAGS = {"Operativo": "operativo", "En reparación": "reparacion",
              "En reparacion": "reparacion", "Baja": "baja"}
@@ -41,37 +46,54 @@ TABLE_CREATION = f'''CREATE TABLE equipos (
                     `sistema_operativo` VARCHAR(255),
                     `modelo_placa_base` VARCHAR(255),
                     `tarjeta_grafica` VARCHAR(255),
+                    `modelo_tarjeta_grafica` VARCHAR(255),
+                    `ram_tarjeta` INTEGER,
                     `tipo_almacenamiento` VARCHAR(255),
-                    `capacidad_almacenamiento` VARCHAR(255),
+                    `capacidad_almacenamiento` INTEGER,
                     `memoria_ram` VARCHAR(255),
-                    `capacidad_ram` VARCHAR(255),
+                    `capacidad_ram` INTEGER,
+                    `usuario_ultima_mod` VARCHAR(255) DEFAULT (USER()),
+                    `fecha_ultima_mod` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                     PRIMARY KEY (`id`)
-                    ) ENGINE=InnoDB'''
+                    ) ENGINE=InnoDB;
+                    
+                    DELIMITER //
+                    CREATE TRIGGER equipos_before_update
+                    BEFORE UPDATE ON equipos
+                    FOR EACH ROW
+                    BEGIN
+                        SET NEW.usuario_ultima_mod = USER();
+                    END;
+                    //
+                    DELIMITER;'''
 
 ADD_ENTRY = '''INSERT INTO `equipos`
                (`nombre`, `marca`, `modelo`, `serial`,
                `fecha_adquisicion`, `estado`, `ubicacion`,
                `sistema_operativo`, `modelo_placa_base`,
-               `tarjeta_grafica`, `tipo_almacenamiento`,
+               `tarjeta_grafica`, `modelo_tarjeta_grafica`,
+               `ram_tarjeta`, `tipo_almacenamiento`,
                `capacidad_almacenamiento`, `memoria_ram`,
                `capacidad_ram`)
-               VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+               VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
 
 UPDATE_ENTRY = '''UPDATE `equipos` SET
-        nombre=%s,
-        marca=%s,
-        modelo=%s,
-        serial=%s,
-        fecha_adquisicion=%s,
-        estado=%s,
-        ubicacion=%s,
-        sistema_operativo=%s,
-        modelo_placa_base=%s,
-        tarjeta_grafica=%s,
-        tipo_almacenamiento=%s,
-        capacidad_almacenamiento=%s,
-        memoria_ram=%s,
-        capacidad_ram=%s
+        `nombre`=%s,
+        `marca`=%s,
+        `modelo`=%s,
+        `serial`=%s,
+        `fecha_adquisicion`=%s,
+        `estado`=%s,
+        `ubicacion`=%s,
+        `sistema_operativo`=%s,
+        `modelo_placa_base`=%s,
+        `tarjeta_grafica`=%s,
+        `modelo_tarjeta_grafica`=%s,
+        `ram_tarjeta`=%s,
+        `tipo_almacenamiento`=%s,
+        `capacidad_almacenamiento`=%s,
+        `memoria_ram`=%s,
+        `capacidad_ram`=%s
         WHERE `id`=%s
 '''
 
