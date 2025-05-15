0fdf71b1a0a63a6074ee7d177d57160db348041a
AgustinAmata <agus.amata2002@gmail.com>
2025-05-15 19:02:28 +0200

    Update DeskBase.py

--- DeskBase.py
+++ DeskBase.py
@@ -6,16 +6,12 @@ from DeskBase_logging import LOGGING_INFO
 logging.config.dictConfig(LOGGING_INFO)
 logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
+def main():
     app = MainApp()
-
-    window_width = 1080
-    window_height = 720
-    position_right = app.winfo_screenwidth()//2 - window_width/2
-    position_down = app.winfo_screenheight()//2 - window_height/2
-    app.geometry(f"{window_width}x{window_height}+{position_right}+{position_down}")
-
     logger.info("DeskBase initialized")
 
     app.mainloop()
-    logger.info("DeskBase destroyed")
+    logger.info("DeskBase destroyed")    
+
+if __name__ == "__main__":
+    main()
