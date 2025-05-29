a5581aa3f1e86cd7b720151344bed484598ea7e4
AgustinAmata <agus.amata2002@gmail.com>
2025-05-29 18:05:59 +0200

    Update login.py

--- src/ui_components/login.py
+++ src/ui_components/login.py
@@ -1,13 +1,20 @@
 import customtkinter as ctk
 import matplotlib.pyplot as plt
+import os
+import sys
 from tkinter import messagebox
 from src.data_controller import db_showentries
 from src.db_manager import DBManager
 from src.logic import CenterWindowToDisplay
 
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    return os.path.join(base_path, relative_path)
+
 class LoginWindow(ctk.CTkToplevel):
     def __init__(self, master, db: DBManager, db_tab):
         super().__init__(master)
+        self.after(250, lambda: self.iconbitmap(resource_path("assets/DeskBase_icon.ico")))
         self.master = master
         self.db = db
         self.db_tab = db_tab
